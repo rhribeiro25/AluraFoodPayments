42e19af41f05311d8640fcf9198cc88cffea066c
rhribeiro25 <renan.shaolin.25@gmail.com>
2024-08-25 16:53:50 +0100

    changing database from mysql to postgres

--- src/main/resources/db/migration/V1__create_payment_table.sql
+++ src/main/resources/db/migration/V1__create_payment_table.sql
@@ -1,12 +1,11 @@
 CREATE TABLE payments (
- id bigint(20) NOT NULL AUTO_INCREMENT,
+ id bigint constraint pk_id_payment PRIMARY KEY,
  pay_value decimal(19,2) NOT NULL,
  pay_name varchar(100) DEFAULT NULL,
  pay_number varchar(19) DEFAULT NULL,
  pay_expiration varchar(7) DEFAULT NULL,
  pay_code varchar(3) DEFAULT NULL,
  pay_status varchar(255) NOT NULL,
- payment_method_id bigint(20) NOT NULL,
- order_id bigint(20) NOT NULL,
-PRIMARY KEY (id)
+ payment_method_id bigint NOT NULL,
+ order_id bigint NOT NULL
 );
